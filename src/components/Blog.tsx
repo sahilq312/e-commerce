dacf8077b7a6d99812e247c1aa555237d0d6e136
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-09-09 17:31:26 +0530

    redux testing done
    learn and trying redux in typescript
    how to manage global state . also saw the difference in javascript and typeScript to use redux

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -1,6 +1,15 @@
+import {  useAppDispatch, useAppSelector } from "../app/hooks"
+import {  decrement, increment , incrementByAmt, selectCount } from "./blogSlice"
 
 export const Blog = () => {
+    const count = useAppSelector(selectCount);
+    const dispatch = useAppDispatch();
   return (
-    <div>blog</div>
+    <div>
+        <h1>{count}</h1>
+        <button onClick={()=> dispatch(increment())}> increment</button>
+        <button onClick={()=> dispatch(decrement())}> decrement</button>
+        <button onClick={()=> dispatch(incrementByAmt(10))}> increment by 10</button>
+    </div>
   )
 }
