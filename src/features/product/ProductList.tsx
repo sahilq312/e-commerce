e817a2f05d0f2c75fc08c1a4d9dd9607b701a563
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-09-10 18:06:17 +0530

    commit

--- src/features/product/ProductList.tsx
+++ src/features/product/ProductList.tsx
@@ -14,7 +14,7 @@ export const ProductList = () => {
     <div className="bg-white">
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="sr-only">Products</h2>
-        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-2 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
           {product.map((product) => (
             <Link key={product.id} to={product.id} className="group">
               <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
