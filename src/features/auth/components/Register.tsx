bf64cae9e589c7626afb5c22a833344fe9601206
sahilq312 <120485501+sahilq312@users.noreply.github.com>
2023-09-21 21:38:01 +0530

    form ui

--- src/features/auth/components/Register.tsx
+++ src/features/auth/components/Register.tsx
@@ -1,7 +1,31 @@
+import { useForm, Resolver } from "react-hook-form";
 
+type FormValues = {
+  firstName: string;
+  email: string;
+  password: string;
+};
 
+const resolver: Resolver<FormValues> = async (values) => {
+  return {
+    values: values.firstName ? values : {},
+    errors: !values.firstName
+      ? {
+          firstName: {
+            type: "required",
+            message: "This is required.",
+          },
+        }
+      : {},
+  };
+};
 export const Register = () => {
-  
+  const {
+    register,
+    handleSubmit,
+    formState: { errors },
+  } = useForm<FormValues>({ resolver });
+  const onSubmit = handleSubmit((data) => console.log(data));
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
@@ -21,17 +45,29 @@ export const Register = () => {
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
               Sign in to your account
             </h1>
-            <form className="space-y-4 md:space-y-6" action="#">
+            <form className="space-y-4 md:space-y-6" onSubmit={onSubmit}>
               <div>
                 <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
-                  Your email
+                  Name
+                </label>
+                <input
+                  type="text"
+                  {...register("firstName")}
+                  placeholder="name"
+                  className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                />
+                {errors?.firstName && <p>{errors.firstName.message}</p>}
+              </div>
+              <div>
+                <label className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                  Email
                 </label>
                 <input
                   type="email"
-                  name="email"
                   id="email"
+                  {...register("email")}
+                  placeholder="email"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-                  placeholder="name@company.com"
                 />
               </div>
               <div>
@@ -40,9 +76,9 @@ export const Register = () => {
                 </label>
                 <input
                   type="password"
-                  name="password"
                   id="password"
-                  placeholder="••••••••"
+                  {...register("password")}
+                  placeholder="password"
                   className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                 />
               </div>
